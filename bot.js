dd5d8ec925acb7413aef582d317bff87ecfdff45
Krzymen <37049328+Krzymen@users.noreply.github.com>
2018-10-26 20:45:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -391,7 +391,7 @@ function addXP(){
 	{
 		const args = msg.content.split(" ").slice(1);
 		const code = args.join(" ");
-		var matma = math.eval(code);
+		var matma = Math.eval(code);
 		if(matma){ 
 			msg.channel.send(matma);
 		}else{
